5c5a251b00ffbe3dd1009f5872fd9ee8e176f74f
victor-arana <victor_arana@hotmail.com>
2020-11-09 11:48:30 -0600

    :configuration::initialize: initialize project
    Changes committed:
    	new file:   .gitignore
    	new file:   pom.xml
    	new file:   src/main/java/no/data/loggingexample/LoggingExampleApplication.java
    	new file:   src/main/resources/application.properties
    	new file:   src/test/java/no/data/loggingexample/LoggingExampleApplicationTests.java

---
+++ src/test/java/no/data/loggingexample/LoggingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package no.data.loggingexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
